c520516f1435dad9334c702fd4e3c11a050d3cb4
Devi Darabathula <devidarabathula@gmail.com>
2026-04-07 16:05:43 +0530

    Switched to SMTP Port 465 SSL for better Render compatibility

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -159,9 +159,9 @@ CORS_ALLOW_ALL_ORIGINS = True # Change in production
 # Email Settings (for OTP)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = 'devidarabathula@gmail.com'
 EMAIL_HOST_PASSWORD = 'xqnolupixahmffjc'
 DEFAULT_FROM_EMAIL = 'devidarabathula@gmail.com'
